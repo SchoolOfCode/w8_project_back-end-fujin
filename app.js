6e8db48a6c1bf3632880301bd08fbf4ed193fb68
Lizard Morrow <lizardmorrow@gmail.com>
2021-09-14 19:46:41 +0100

    Add engines section to package.json

--- app.js
+++ app.js
@@ -5,18 +5,19 @@ var logger = require('morgan');
 
 var app = express();
 
-const airportsRouter = require("./routes/airports");
-const airlinesRouter = require("./routes/airlines");
+const airportsRouter = require('./routes/airports');
+const airlinesRouter = require('./routes/airlines');
 const flightsRouter = require('./routes/flightInfo');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', (req, res) => res.json({ message: 'Hello world!' }));
 app.use('/flights', flightsRouter);
-app.use("/airports", airportsRouter);
-app.use("/airlines", airlinesRouter);
+app.use('/airports', airportsRouter);
+app.use('/airlines', airlinesRouter);
 
 module.exports = app;
