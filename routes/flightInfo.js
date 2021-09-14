4f5b019df844576652834b056a509ae6281dfecd
valerio <romavalerioio@gmail.com>
2021-09-14 15:49:12 +0100

    removed serviceTypeCode from cleanData

--- routes/flightInfo.js
+++ routes/flightInfo.js
@@ -1,17 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const axios = require('axios');
+const axios = require("axios");
+const dataFilter = require("../helpers/dataFilter");
 
 /* GET home page. */
-router.get('/', async (req, res) => {
-  const response = await axios.get(
-      "https://api.oag.com/flights/?Limit=5",
-      {headers: {"Subscription-Key": "251562bba0704e11b8a9fad1342c6ea6", "Cache-Control": "no-cache"
-    }}
-  );
-  const rawData = await response.data;
-  // const cleanData = 
-  res.json(cleanData);
-  })
+router.get("/", async (req, res) => {
+  const response = await axios.get("https://api.oag.com/flights/?Limit=5", {
+    headers: { "Subscription-Key": "251562bba0704e11b8a9fad1342c6ea6" },
+  });
+  const rawData = await response.data.data;
+  res.json(dataFilter(rawData));
+});
 
 module.exports = router;
