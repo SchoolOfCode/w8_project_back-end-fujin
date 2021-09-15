55e19d27f05b713ff83d4c13af6e09baa53bf9fb
valerio <romavalerioio@gmail.com>
2021-09-15 09:46:32 +0100

    day 2

--- routes/flightInfo.js
+++ routes/flightInfo.js
@@ -3,13 +3,28 @@ var router = express.Router();
 const axios = require("axios");
 const dataFilter = require("../helpers/dataFilter");
 
-/* GET home page. */
 router.get("/", async (req, res) => {
-  const response = await axios.get("https://api.oag.com/flights/?Limit=5", {
-    headers: { "Subscription-Key": "251562bba0704e11b8a9fad1342c6ea6" },
-  });
-  const rawData = await response.data.data;
-  res.json(dataFilter(rawData));
+  const { departureAirport, arrivalAirport, date } = req.query;
+  const response = await axios.get( // move to helper functions
+    `https://api.oag.com/flights/?DepartureAirport=${departureAirport}&ArrivalAirport=${arrivalAirport}&DepartureDate=${date}`, // params
+    {
+      headers: { "Subscription-Key": "251562bba0704e11b8a9fad1342c6ea6" }, //environment variable
+    }
+  );
+  const rawData = await response.data.data; // error handling
+  const cleanData = dataFilter(rawData);
+  if (cleanData.length === 0) { // === 0 {}
+    res.status(404).json({
+      success: false,
+      message: "No suitable flights available",
+    });
+  } else {
+    res.status(200).json({
+      success: true,
+      message: `List of suitable flights from the airport ${departureAirport} to the airport ${arrivalAirport} on the date ${date}`,
+      payload: cleanData,
+    });
+  }
 });
 
 module.exports = router;
